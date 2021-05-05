7901f1827a826d4ac44198271a63df1ceabe0cc7
pankajkcodes <pankajkcodes@gmail.com>
2021-05-05 21:56:01 +0530

    Initial commit for sample youtube player

---
+++ app/src/main/java/com/pankajkcodes/youtubevideoplayersample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.pankajkcodes.youtubevideoplayersample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+    String video_id ="gXWXKjR-qII";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = video_id;
+                youTubePlayer.loadVideo(videoId, 0);
+
+            }
+        });
+
+    }
+}
